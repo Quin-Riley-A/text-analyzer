a8c1e5cce6d81f69aa98bcdb5b16ffc69686f481
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-30 21:07:41 -0400

    include filter for words w/o numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ function wordCounter(text){
   if (text.trim().length === 0) {
     return 0;
   }
-  let wordcount = 0;
-  const textArr = text.split(" ");
-textArr.forEach(function(word){
-  wordcount++;
-});
-  return wordcount;
-} 
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
