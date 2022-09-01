9b93967b31bf4dd5df6168803608fda83bf79930
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-31 21:55:13 -0400

    added UI and Front end functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,20 @@ function omitBadWords(phrase) {
   }
   return phrase;
 }
+
+
+//UI Logic
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
